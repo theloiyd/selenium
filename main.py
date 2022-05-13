7e19205ef1968950f9054161eaad3ba44a0f0341
theloiyd <leo.ce.03@outlook.de>
2022-05-13 14:58:19 +0200

    h

--- main.py
+++ main.py
@@ -1,14 +1,34 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
+from openpyxl import load_workbook
+from selenium.webdriver.common.keys import Keys
 
+wb = load_workbook(r"C:\users\Leo\Desktop\Mappe1.xlsx")
+sheet_obj = wb.active
+m_row = sheet_obj.max_row
 browser = webdriver.Firefox()
-browser.get("https://www.amazon.de/DualSense-Wireless-Controller-Cosmic-PlayStation/dp/B094WRT8PD/ref=pd_sim_sccl_1_3/260-5777354-5953213?pd_rd_w=3ez3l&pf_rd_p=96527118-ae73-4a18-939d-14eb43bee5ae&pf_rd_r=P9AS1Q9ZY85CP5N24FBD&pd_rd_r=fa82c0f1-a036-4d86-8332-c2e00d6b71cd&pd_rd_wg=yDghL&pd_rd_i=B094WRT8PD&th=1")
-cookieButton = browser.find_element_by_xpath('//*[@id="sp-cc-accept"]')
 
-buyButton = browser.find_element_by_xpath('//*[@id="buy-now-button"]')
-cookieButton.click()
-buyButton.click()
+for i in range(2, m_row + 1):
+    browser.find_element(By.TAG_NAME, 'body').send_keys(Keys.COMMAND + 't')
+    browser.get("https://www.amazon.de/ring-video-doorbell-wired/dp/B08LR3G17D/ref=sr_1_2?keywords=ring&qid=1652445690&sr=8-2")
+    cookieButton = browser.find_element(By.XPATH, '//*[@id="sp-cc-accept"]')
+    buyButton = browser.find_element(By.XPATH, '//*[@id="buy-now-button"]')
+    cookieButton.click()
+    buyButton.click()
+    browser.find_element(By.TAG_NAME, 'body').send_keys(Keys.COMMAND + 'w')
+
+
+
+    email_obj = sheet_obj.cell(row=i, column=1)
+    password_obj = sheet_obj.cell(row=i, column=2)
+
+
+
+
+
+
 
 
 
